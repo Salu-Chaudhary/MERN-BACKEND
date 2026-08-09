581c9a37eeb8fee73d05e6b41e5f7bc9c3757b2b
Salu Chaudhary <saluc6370@gmail.com>
2026-08-09 15:07:20 +0545

    CORS SETUP

--- src/app.js
+++ src/app.js
@@ -5,9 +5,21 @@ import orderRouter from "./routers/order.route.js";
 import authRouter from "./routers/auth.route.js";
 import connectWithDB from "./config/db.js";
 import config from "./config/config.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+const corsOption = {
+  origin: "http://127.0.0.1:5500/index.html", // Allow only this origin
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed custom headers
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOption));
+
+app.get("/api/data", (req, res) => {
+  res.json({ message: "CORS-enabled" });
+});
 
 connectWithDB();
 
